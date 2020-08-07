2fca150a5ca767d78aefa3863955c4de9926ea34
Trisanu <trisanubhar@gmail.com>
2020-08-07 18:05:26 +0530

    Added flutter files

---
+++ android/app/src/main/kotlin/com/example/upitch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.upitch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "UPitch",
+      home: ChangeRoute(),
+    ),
+  );
+}
+
+class ChangeRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
