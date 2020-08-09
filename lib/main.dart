8deb798412b36e7aabfbf16f486419a074cbcfbf
Trisanu <trisanubhar@gmail.com>
2020-08-09 19:36:28 +0530

    Login Functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:upitch/home.dart';
+import 'package:upitch/login_register/switcher.dart';
+import 'package:upitch/shared_resources/authenticate.dart';
 
 void main() {
   runApp(
@@ -13,7 +17,26 @@ void main() {
 class ChangeRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamProvider<CurrentUser>.value(
+      value: Authenticate().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
+
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<CurrentUser>(context);
+    if(user == null){
+      return SwitchScreens();
+    }
+    else{
+      return HomePage();
+    }
   }
 }
 
